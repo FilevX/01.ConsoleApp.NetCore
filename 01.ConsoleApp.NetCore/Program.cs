57a7b69cdf9925ad0dd908712571eb14cfdf98ee
filevx <filevx@gmail.com>
2017-10-06 16:17:06 +0300

    Добавьте файлы проекта.

---
+++ 01.ConsoleApp.NetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.ConsoleApp.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
