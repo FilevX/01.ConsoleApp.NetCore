563408d29e3f284e1739dc8e7109375c7cedd1a3
filevx <filevx@gmail.com>
2017-10-06 16:23:32 +0300

    Init

--- 01.ConsoleApp.NetCore/Program.cs
+++ 01.ConsoleApp.NetCore/Program.cs
@@ -7,6 +7,7 @@ namespace _01.ConsoleApp.NetCore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
